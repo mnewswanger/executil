71d9224afa404edb7137186a2ae6970ce31fc5ab
Mike Newswanger <mike@mikenewswanger.com>
2017-06-07 12:06:48 -0400

    Remove dependency on docker bin for tests

--- executil_test.go
+++ executil_test.go
@@ -7,8 +7,11 @@ import (
 func TestMe(t *testing.T) {
 	var c = Command{
 		Name:       "Test function output",
-		Executable: "docker",
-		Verbosity:  4,
+		Executable: "echo",
+		Arguments: []string{
+			"It works!",
+		},
+		Verbosity: 4,
 	}
 	if err := c.Run(); err != nil {
 		t.Error("Command failed to execute")
